6b0fdd34b71124e63b47d7f20607b8b61c3347b5
Kam300 <kamilgaripov205@gmail.com>
2024-12-09 01:06:26 +0500

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Input;
+
+namespace WpfApp3
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MyTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (Keyboard.IsKeyDown(Key.LeftShift) || Keyboard.IsKeyDown(Key.RightShift))
+            {
+                if (e.Key == Key.F12)
+                {
+                    MessageBox.Show("Нажата комбинация Shift + F12!");
+                }
+            }
+        }
+
+        private void PhoneTextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            // Проверяем, что вводимые символы - это цифры, тире или пробел
+            if (!Regex.IsMatch(e.Text, @"^[0-9\-\s]+$"))
+            {
+                e.Handled = true; // Отклонить ввод
+            }
+        }
+
+        private void PhoneTextBox_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            // Позволяют Backspace и Delete
+            if (e.Key == Key.Back || e.Key == Key.Delete)
+            {
+                return; // Позволить эти клавиши
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 newe= new Window1();
+            newe.Show();
+            this.Hide();
+        }
+    }
+}
---
+++ Window1.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Shapes; //Rectangle
+
+namespace WpfApp3
+{
+    public partial class Window1 : Window
+    {
+        private bool isDragging = false;
+        private Point clickPosition;
+        private UIElement draggingElement;
+
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void Canvas_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            // Нач перетас
+            if (e.OriginalSource is Rectangle rectangle)
+            {
+                isDragging = true;
+                draggingElement = rectangle;
+                clickPosition = e.GetPosition(rectangle);
+                Mouse.Capture(rectangle); // Захват мышь
+            }
+        }
+
+        private void Canvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDragging && draggingElement != null)
+            {
+                // Перемещение 
+                Point mousePosition = e.GetPosition(canvas);
+                double left = mousePosition.X - clickPosition.X;
+                double top = mousePosition.Y - clickPosition.Y;
+
+                // Ограничение 
+                if (left < 0) left = 0;
+                if (top < 0) top = 0;
+                if (left + ((Rectangle)draggingElement).Width > canvas.ActualWidth)
+                    left = canvas.ActualWidth - ((Rectangle)draggingElement).Width;
+                if (top + ((Rectangle)draggingElement).Height > canvas.ActualHeight)
+                    top = canvas.ActualHeight - ((Rectangle)draggingElement).Height;
+
+                Canvas.SetLeft(draggingElement, left);
+                Canvas.SetTop(draggingElement, top);
+            }
+        }
+
+        private void Canvas_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            // конец перетаскивание
+            if (isDragging)
+            {
+                isDragging = false;
+                draggingElement = null;
+                Mouse.Capture(null); // Освоб захв мыши
+            }
+        }
+    }
+}
